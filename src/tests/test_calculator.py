8f5513886a1fc76dca5102f81e7bde70778108f0
Yves Ndoky <scorpiondoky@gmail.com>
2025-09-13 00:14:55 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -9,5 +9,11 @@ from calculator import Calculator
 def test_app():
     my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
-
+    assert my_calculator.addition(2, 3) == 5
+    assert my_calculator.subtraction(6,2) == 4
+    assert my_calculator.multiplication(2,2) == 4
+    assert my_calculator.division(6,2) == 3
+    assert my_calculator.division(6,0) == "Erreur : division par zéro"
+    assert my_calculator.addition(5,6) == 43 #test renvoyant volontairement une erreur 
+    
 # TODO: ajoutez les tests
